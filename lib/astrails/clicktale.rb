effbaca69c5544d8bef54405540b21df6b81863a
helpdeskdev <helpdesk-dev@locaweb.com.br>
2010-10-29 14:42:00 -0200

    fixing clicktale initialization to work as a gem in Rails 3

--- lib/astrails/clicktale.rb
+++ lib/astrails/clicktale.rb
@@ -12,9 +12,9 @@ module Astrails
 
     CONFIG = HashWithIndifferentAccess.new
     begin
-      conffile = File.join(RAILS_ROOT, "config", "clicktale.yml")
+      conffile = File.join(Rails.root.to_s, "config", "clicktale.yml")
       conf = YAML.load(File.read(conffile))
-      CONFIG.merge!(conf[RAILS_ENV])
+      CONFIG.merge!(conf[Rails.env])
     rescue
       puts "*" * 50
       puts "#{conffile} can not be loaded:"
